613c2af3aa3f3947e756ed1a03422800023d2d1c
Konstantinos Lampropoulos <KOSTAS@192.168.1.157>
2023-02-14 01:29:16 +0200

    finished TwoDTree file

--- TwoDTree.java
+++ TwoDTree.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class TwoDTree { 
     private Treenode head; //root of the tree
@@ -99,16 +101,49 @@ public class TwoDTree {
     } // does the tree contain p?
 
     //TODO:  Fix thoe methods
-    /* 
+    
     public Point nearestNeighbor(Point p){
         if (isEmpty()){
             return null;
         }
-        
+        Point nearest = new Point(head.get_point().get_x(), head.get_point().get_y());
+        double nearest_distance = p.distanceTo(nearest);
+
+        recursive_nearestNeighbor(nearest, nearest_distance, p, head);
+
+        return nearest;
     } // point in the tree that is closest to p (null if tree is empty)
+
+    public void recursive_nearestNeighbor(Point nearest, double nearest_distance, Point p, Treenode Head){
+        if(head.get_l() == null || head.get_r() == null){
+            return;
+        }
+        Rectangle head_rect = new Rectangle(head.get_l().get_point().get_x(), head.get_r().get_point().get_x(), head.get_l().get_point().get_y(),  head.get_r().get_point().get_y());
+        if (head_rect.distanceTo(p)<nearest_distance){
+            nearest = head.get_point();
+            nearest_distance = p.distanceTo(head.get_point());
+
+            recursive_nearestNeighbor(nearest, nearest_distance, p, head.get_l());
+            recursive_nearestNeighbor(nearest, nearest_distance, p, head.get_r());
+        }
+    }
     
     public List<Point> rangeSearch(Rectangle rect){
-
+        List<Point> return_list = new ArrayList<Point>();
+        recursive_rangeSearch(rect, head, return_list);
+        return return_list;
     } // Returns a list with the Points that are contained in the rectangle
-    */
+    
+    public void recursive_rangeSearch(Rectangle rect, Treenode head, List<Point> list){
+        if(head.get_l() == null || head.get_r() == null){
+            return;
+        }
+        Rectangle head_rect = new Rectangle(head.get_l().get_point().get_x(), head.get_r().get_point().get_x(), head.get_l().get_point().get_y(),  head.get_r().get_point().get_y());
+        if(!head_rect.intersects(rect)){
+            return;
+        }
+        list.add(head.get_point());
+        recursive_rangeSearch(rect, head.get_l(), list);
+        recursive_rangeSearch(rect, head.get_r(), list);
+    }
 }
