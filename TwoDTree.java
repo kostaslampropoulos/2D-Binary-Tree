565d4559ca26b97fca3afa7dd9d913050f840b67
Konstantinos Lampropoulos <lampropoulos.kwnstantinos@gmail.com>
2023-02-17 21:06:11 +0200

    finished 1 TODO

--- TwoDTree.java
+++ TwoDTree.java
@@ -1,5 +1,12 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.print.event.PrintEvent;
 
 public class TwoDTree { 
     private Treenode head; //root of the tree
@@ -113,18 +120,31 @@ public class TwoDTree {
         return nearest;
     } // point in the tree that is closest to p (null if tree is empty)
 
-    public void recursive_nearestNeighbor(Point nearest, double nearest_distance, Point p, Treenode Head){
-        if(head.get_l() == null || head.get_r() == null){
+    public void recursive_nearestNeighbor(Point nearest, double nearest_distance, Point p, Treenode head){
+
+        if(head == null ){
             return;
         }
-        Rectangle head_rect = new Rectangle(head.get_l().get_point().get_x(), head.get_r().get_point().get_x(), head.get_l().get_point().get_y(),  head.get_r().get_point().get_y());
-        if (head_rect.distanceTo(p)<nearest_distance){
-            nearest = head.get_point();
-            nearest_distance = p.distanceTo(head.get_point());
 
-            recursive_nearestNeighbor(nearest, nearest_distance, p, head.get_l());
-            recursive_nearestNeighbor(nearest, nearest_distance, p, head.get_r());
+        if(head.get_l() != null && head.get_r() != null){
+            int x_max = Math.max(head.get_l().get_point().get_x(), head.get_r().get_point().get_x());
+            int x_min = Math.min(head.get_l().get_point().get_x(), head.get_r().get_point().get_x());
+    
+            int y_max = Math.max(head.get_l().get_point().get_y(), head.get_r().get_point().get_y());
+            int y_min = Math.max(head.get_l().get_point().get_y(), head.get_r().get_point().get_y());
+            
+            Rectangle head_rect = new Rectangle(x_min, x_max, y_min, y_max);
+        
+            if (head_rect.distanceTo(p) < nearest_distance){
+                
+                nearest = head.get_point();
+                nearest_distance = p.distanceTo(head.get_point());
+
+                recursive_nearestNeighbor(nearest, nearest_distance, p, head.get_l());
+                recursive_nearestNeighbor(nearest, nearest_distance, p, head.get_r());
+            }
         }
+        return;
     }
     
     public List<Point> rangeSearch(Rectangle rect){
@@ -134,7 +154,7 @@ public class TwoDTree {
     } // Returns a list with the Points that are contained in the rectangle
     
     public void recursive_rangeSearch(Rectangle rect, Treenode head, List<Point> list){
-        if(head.get_l() == null || head.get_r() == null){
+        if(head == null){
             return;
         }
         Rectangle head_rect = new Rectangle(head.get_l().get_point().get_x(), head.get_r().get_point().get_x(), head.get_l().get_point().get_y(),  head.get_r().get_point().get_y());
@@ -146,7 +166,49 @@ public class TwoDTree {
         recursive_rangeSearch(rect, head.get_r(), list);
     }
 
+    public void print(Treenode head){
+        if (head == null){
+            return;
+        }
+        System.out.println(head.get_point());
+        if(head.get_l() != null){
+            System.out.println("Left child of " + head.get_point());
+            print(head.get_l());
+        }
+        if(head.get_r() != null){
+            System.out.println("Right child of " + head.get_point());
+            print(head.get_r());
+        }
+        return;
+    }
+    
+
     public static void main (String[] args){
-        //TODO
+        BufferedReader reader = null;
+        String l, path = args[0];
+
+        try {
+            reader = new BufferedReader(new FileReader(new File(path)));
+            l = reader.readLine();
+            int num_points = Integer.parseInt(l.trim());
+
+            TwoDTree tree = new TwoDTree();
+            
+            for(int i=0; i<num_points; i++){
+                l = reader.readLine();
+                l = l.trim();
+                String [] coo = l.split(" ");
+                
+                int x = Integer.parseInt(coo[0]);
+                int y = Integer.parseInt(coo[1]);
+                
+                tree.insert(new Point(x,y));   
+            }
+            
+            //tree.print(tree.head);
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
